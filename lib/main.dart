1248f43a946d057e0e611c1825147812247f81a6
RadenDimasErlangga21 <zirkified123@gmail.com>
2023-09-26 15:02:40 +0700

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:crud_sederhana_raden/src/app.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:crud_sederhana_raden/screens/login.dart'; // Import your Login screen
+import 'package:crud_sederhana_raden/services/user_data_manager.dart';
 
 
 //Routes pada Aplikasi
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await UserDataManager().initDatabase(); // Initialize the database
   // Initialize sqflite_common_ffi
   sqfliteFfiInit();
 
